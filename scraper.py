779ca71e6f76e3df87f9dfafe04ce4e4225a583c
Hannah Provenza <hprovenza@gmail.com>
2016-04-22 10:07:22 -0400

    Added scraper.py

---
+++ scraper.py
@@ -0,0 +1,39 @@
+__author__ = 'hannahprovenza'
+from bs4 import BeautifulSoup, SoupStrainer
+import lxml
+import requests
+
+url = 'http://tvtropes.org/pmwiki/pmwiki.php/Main/SignificantGreenEyedRedhead'
+html_doc = requests.get(url)
+soup = BeautifulSoup(html_doc.content, 'lxml')
+
+page_data = {}
+#article text
+page_data["text"] = soup.find("div", class_="page-content").get_text()
+
+#title string
+page_data["title"] = soup.title.string
+
+#creates list of categories
+category_list = []
+for media_category in soup.find_all("div", class_="folderlabel", ):
+    category_list.append(media_category.get_text().strip())
+category_list.remove(category_list[0])
+
+
+for media_category, media_details in zip(category_list, soup.find_all("div", class_="folder")):
+    page_data[media_category] = {}
+    for list_item in media_details.find_all('li'):
+        try:
+            link = list_item.find('a')
+            if link.get('href').startswith('http://tvtropes.org') and not link.get('href').startswith('http://tvtropes.org/pmwiki/pmwiki.php/Main'):
+                page_data[media_category][link.get_text()] = link.get('href')
+        except TypeError:
+            continue
+        except AttributeError:
+            continue
+
+# preview output
+# import pprint
+# pp = pprint.PrettyPrinter(indent=4)
+# pp.pprint(page_data)
